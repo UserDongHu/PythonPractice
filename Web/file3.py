a44011de4d866c1db666323e8298104361ea753c
KimDongHu <ehdgn1022@naver.com>
2023-07-10 23:05:34 +0900

    BeautifulSoup 연습

---
+++ Web/file3.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import urllib.request as req
+
+res = req.urlopen("http://127.0.0.1:5500/Web/index.html")
+soup = BeautifulSoup(res, 'html.parser')
+print(soup.find('img')['src'])
+for tag in soup.find_all('a'):
+    print(tag['href'])
+print(soup.select('#test1 > img'))
